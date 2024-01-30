66963e144ce4265d32a2b1343c50bf3013a5e9b5
ViniciusROReis07 <guaguinhireis@gmail.com>
2024-01-30 20:52:09 -0300

    feat: authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,39 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/setup', function(Request $request){
+    $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => 'password'
+    ];
+
+
+    if(!Auth::attempt($credentials)){
+        $user = new \App\Models\User();
+
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
+
+        $user->save();
+
+        if(Auth::attempt($credentials)){
+            
+            $user = $request->user();
+
+            $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+            $updateToken = $user->createToken('update-token', ['create', 'update', 'delete']);
+            $basicToken = $user->createToken('basic-token', ['create', 'update', 'delete']);
+
+            return [
+                'admin' => $adminToken->plainTextToken,
+                'update' => $updateToken->plainTextToken,
+                'basic' => $basicToken->plainTextToken,
+            ];
+        }
+
+    }
+
 });
